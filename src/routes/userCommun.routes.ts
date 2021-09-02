5130d3daa708709d07b1e8a8992adcfa692deb9a
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-02 19:51:24 -0300

    feat: :sparkles: create connection with db

---
+++ src/routes/userCommun.routes.ts
@@ -0,0 +1,14 @@
+import { Request, Response, Router } from "express";
+import { getRepository } from "typeorm";
+import { CommunUser } from "../entity/communUser";
+
+const userCommunRouter = Router();
+
+userCommunRouter.post("/", async (req: Request, res: Response) => {
+  const repository = getRepository(CommunUser);
+  const userToSave = repository.create(req.body);
+  const user = await repository.save(userToSave);
+  return res.status(201).json(user);
+});
+
+export default userCommunRouter;
