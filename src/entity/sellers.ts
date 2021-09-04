5751c3109e191c923ce18915362b81fdd0bf50c0
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-04 19:54:47 -0300

    feat: :sparkles: add entity for sellers

---
+++ src/entity/sellers.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("sellers")
+export class Sellers {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  complete_name: string;
+
+  @Column()
+  cpf_cnpj: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  senha: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  update_at: Date;
+}
