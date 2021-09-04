5c05eee8fcdcf91fa2345916b238cb3da870e187
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-04 19:22:36 -0300

    feat: :sparkles: introduce a new feature for verify user already exists

--- src/controllers/userCommun.ts
+++ src/controllers/userCommun.ts
@@ -5,6 +5,13 @@ import { CommunUser } from "../entity/communUser";
 class UserCommunController {
   async store(req: Request, res: Response) {
     const repository = getRepository(CommunUser);
+    const { email, cpf } = req.body;
+    const userExists = await repository.findOne({ where: { email, cpf } });
+    if (userExists) {
+      return res.send(
+        "Email is already registered in the system"
+      );
+    }
     const userToSave = repository.create(req.body);
     const user = await repository.save(userToSave);
     return res.status(201).json(user);
