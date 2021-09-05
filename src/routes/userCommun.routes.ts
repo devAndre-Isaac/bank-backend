0629ec7054575ec27b1031cd660de3cecd433256
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-05 18:13:05 -0300

    feat: :sparkles: insert validation in user commun route

--- src/routes/userCommun.routes.ts
+++ src/routes/userCommun.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import { userValidator } from "../validator/validation";
 
 import UserCommunController from "../controllers/userCommun";
 
 const userCommunRouter = Router();
 
-userCommunRouter.post("/picpay/user", UserCommunController.store );
+userCommunRouter.post(
+  "/picpay/user",
+  userValidator,
+  UserCommunController.store
+);
 
 export { userCommunRouter };
