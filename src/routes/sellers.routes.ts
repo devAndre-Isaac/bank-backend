72daff3e0c3054426f8665f636213efa9709b451
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-05 18:12:50 -0300

    feat: :sparkles: insert validation in sellers route

--- src/routes/sellers.routes.ts
+++ src/routes/sellers.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import SellersController from "../controllers/sellers";
+import { userValidator } from "../validator/validation";
 
 const sellersRouter = Router();
 
-sellersRouter.post("/picpay/sellers", SellersController.store );
+sellersRouter.post("/picpay/sellers", userValidator, SellersController.store);
 
 export { sellersRouter };
