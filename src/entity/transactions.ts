71512dc851e068ff98b50088ace62741712e35a4
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-08 20:48:02 -0300

    feat: :sparkles: add transactions entity

---
+++ src/entity/transactions.ts
@@ -0,0 +1,10 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity("transactions")
+export class Transactions {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  value: string;
+}
