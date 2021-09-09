26180a01d9b4ac7a08748c6e6f73b98e94604a9e
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-09 17:48:29 -0300

    refactor: :recycle: refactor admin controller

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -6,8 +6,8 @@ import { Sellers } from "../entity/sellers";
 class AdminController {
   async readSeller(req: Request, res: Response) {
     const repositorySellers = getRepository(Sellers);
-    const sellerToRead = await repositorySellers.findAndCount();
-    return res.json({ sellers: sellerToRead });
+    const sellersToRead = await repositorySellers.findAndCount();
+    return res.json({ sellers: sellersToRead });
   }
   async readUser(req: Request, res: Response) {
     const repositoryUsers = getRepository(CommunUser);
