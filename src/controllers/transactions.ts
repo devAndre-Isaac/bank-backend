8c2c1c7a3c8ab39d568d011d70abc14713fadde8
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-14 18:52:23 -0300

    feat: :sparkles: add wallet logical controller

--- src/controllers/transactions.ts
+++ src/controllers/transactions.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
+import { Any, getRepository } from "typeorm";
 import { Transactions } from "../entity/transactions";
+import { CommunUser } from "../entity/communUser";
 
 class TransactionsController {
   async store(req: Request, res: Response) {
-    const repository = getRepository(Transactions);
+    const repository = getRepository(CommunUser);
+    const { wallet } = req.body;
+    const walletTem = repository.findOne({ where: { wallet } });
+    if (wallet > walletTem) {
+      return res.status(400).json({ message: "Insufficient funds" });
+    }
     const transactionsToSave = repository.create(req.body);
+
     const transactions = await repository.save(transactionsToSave);
     return res.status(201).json(transactions);
   }
