8ff2b47e7168a586f3ea2e572d6b49dde03b2d65
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-24 17:08:01 -0300

    refactor: :recycle: change for getMongoRepository

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -1,22 +1,22 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
+import { getMongoRepository } from "typeorm";
 
 import { CommunUser } from "../entity/communUser";
 import { Sellers } from "../entity/sellers";
 
 class AdminController {
   async readSeller(req: Request, res: Response) {
-    const repositorySellers = getRepository(Sellers);
+    const repositorySellers = getMongoRepository(Sellers);
     const sellersToRead = await repositorySellers.findAndCount();
     return res.json({ sellers: sellersToRead });
   }
   async readUser(req: Request, res: Response) {
-    const repositoryUsers = getRepository(CommunUser);
+    const repositoryUsers = getMongoRepository(CommunUser);
     const usersToRead = await repositoryUsers.findAndCount();
     return res.json({ users: usersToRead });
   }
   async readUserById(req: Request, res: Response) {
-    const repositoryUsersId = getRepository(CommunUser);
+    const repositoryUsersId = getMongoRepository(CommunUser);
     const { _id } = req.params;
     const idReturn = await repositoryUsersId.findOne(_id);
     if (!idReturn) {
