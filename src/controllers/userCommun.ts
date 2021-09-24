9845f38479f2c279068dece96b2125438ea4aafb
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-24 17:08:54 -0300

    refactor: :recycle: change for getMongoRepository

--- src/controllers/userCommun.ts
+++ src/controllers/userCommun.ts
@@ -1,17 +1,15 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
+import { getMongoRepository } from "typeorm";
 
 import { CommunUser } from "../entity/communUser";
 
 class UserCommunController {
   async store(req: Request, res: Response) {
-    const repository = getRepository(CommunUser);
+    const repository = getMongoRepository(CommunUser);
     const { email, cpf } = req.body;
     const userExists = await repository.findOne({ where: { email, cpf } });
     if (userExists) {
-      return res.send(
-        "Already registered in the system"
-      );
+      return res.send("Already registered in the system");
     }
     const userToSave = repository.create(req.body);
     const user = await repository.save(userToSave);
