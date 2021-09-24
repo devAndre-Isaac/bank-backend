00924f01a40cc4975e8712bfd7890a67de4015a8
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-24 17:07:33 -0300

    refactor: :recycle: refactor server

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,18 @@
 import "reflect-metadata";
 import express from "express";
+import "./database";
 
-import { connection } from "./database";
 import { userCommunRouter } from "./routes/userCommun.routes";
 import { transactionsRouter } from "./routes/transactions.routes";
 import { sellersRouter } from "./routes/sellers.routes";
 import { adminRouter } from "./routes/admin.routes";
 
-async function serverOn() {
-  await connection();
+const app = express();
 
-  const app = express();
+app.use(express.json());
 
-  app.use(express.json());
+app.use(userCommunRouter, transactionsRouter, sellersRouter, adminRouter);
 
-  app.use(userCommunRouter, transactionsRouter, sellersRouter, adminRouter);
-
-  app.listen(3333, () => {
-    console.log(" ⚡ Server started at http://localhost:3333");
-  });
-}
-serverOn();
+app.listen(3333, () => {
+  console.log(" ⚡ Server started at http://localhost:3333");
+});
