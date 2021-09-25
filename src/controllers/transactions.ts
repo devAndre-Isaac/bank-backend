f9d6606d9eccd4148412558fa4a33745ffb3b908
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-25 19:24:14 -0300

    feat: :sparkles: add controller for transactions system

--- src/controllers/transactions.ts
+++ src/controllers/transactions.ts
@@ -6,9 +6,42 @@ import { CommunUser } from "../entity/communUser";
 class TransactionsController {
   async store(req: Request, res: Response) {
     const repository = getMongoRepository(CommunUser);
+
+    const { sendId } = req.body;
     const { id } = req.params;
-    const walletIdentification = repository.findOne({ where: { id } });
-    console.log(walletIdentification);
+
+    const identificationAccount = await repository.findOne(sendId);
+
+    const carteiraDele = identificationAccount?.wallet as any;
+
+    const walletIdentification = await repository.findOne(id);
+
+    const wallet = walletIdentification?.wallet;
+
+    const { value } = req.body;
+
+    if (carteiraDele < value) {
+      res.send({ Message: "Saldo Insuficiente " });
+    }
+    const subValue = carteiraDele - value;
+    const replaceSubWallet = { ...identificationAccount, wallet: subValue };
+
+    const sumValue = value + wallet;
+    const replaceSumWallet = { ...walletIdentification, wallet: sumValue };
+
+    const subToCreate = repository.create(replaceSubWallet);
+    const subToSave = await repository.save(subToCreate);
+
+    const sumToCreate = repository.create(replaceSumWallet);
+    const sumToSave = await repository.save(sumToCreate);
+
+    return res
+      .status(201)
+      .json([
+        "LogEvento: ",
+        { TransactionsDataOfReceive: sumToSave },
+        { TransactionsDataOfSend: subToSave },
+      ]);
   }
 }
 
