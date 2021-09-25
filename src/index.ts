646e8bc3cec81093aa6acd817b2f6421700a6e7e
devAndre-Isaac <andreisaac344@gmail.com>
2021-09-25 19:22:54 -0300

    feat: :sparkles: add depositRouter in app.use

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,13 @@ import { userCommunRouter } from "./routes/userCommun.routes";
 import { transactionsRouter } from "./routes/transactions.routes";
 import { sellersRouter } from "./routes/sellers.routes";
 import { adminRouter } from "./routes/admin.routes";
+import { depositRouter } from "./routes/deposit.routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.use(userCommunRouter, transactionsRouter, sellersRouter, adminRouter);
+app.use(userCommunRouter, transactionsRouter, sellersRouter, adminRouter, depositRouter);
 
 app.listen(3333, () => {
   console.log(" ⚡ Server started at http://localhost:3333");
