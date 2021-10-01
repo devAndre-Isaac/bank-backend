4c84badaf391d02c3f584f6336f638bc44a2a0a1
devAndre-Isaac <andreisaac344@gmail.com>
2021-10-01 10:22:47 -0300

    style: :art: format code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import "./database";
 
 import { userCommunRouter } from "./routes/userCommun.routes";
@@ -11,14 +11,20 @@ import { depositRouter } from "./routes/deposit.routes";
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.use(userCommunRouter, transactionsRouter, sellersRouter, adminRouter, depositRouter);
+app.use(
+  userCommunRouter,
+  transactionsRouter,
+  sellersRouter,
+  adminRouter,
+  depositRouter
+);
 
-app.get('/', (request, response) => {
-  return response.json({ message: "PayPic - Versão 0.1"})
-})
+app.get("/", (request, response) => {
+  return response.json({ message: "PayPic - Versão 0.1" });
+});
 
 app.listen(3333, () => {
   console.log(" ⚡ Server started at http://localhost:3333");
