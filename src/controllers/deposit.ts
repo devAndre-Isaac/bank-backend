6437c6030af6c866e51b438c9f0187c8c7fd1ba0
devAndre-Isaac <andreisaac344@gmail.com>
2021-10-01 10:23:42 -0300

    feat: :sparkles: insert condition for null values

--- src/controllers/deposit.ts
+++ src/controllers/deposit.ts
@@ -6,14 +6,17 @@ import { CommunUser } from "../entity/users";
 class DepositController {
   async store(req: Request, res: Response) {
     const repository = getMongoRepository(CommunUser);
-    const { id } = req.params
+    const { id } = req.params;
     const { value } = req.body;
     const idExist = await repository.findOne(id);
     if (!idExist) {
       return res.send("ID does not exists");
     }
-    const valueSum = idExist.wallet + value
-    const updateWallet = {...idExist, wallet: valueSum}
+    if (!value) {
+      res.sendStatus(409);
+    }
+    const valueSum = idExist.wallet + value;
+    const updateWallet = { ...idExist, wallet: valueSum };
     const walletToSave = repository.create(updateWallet);
     const wallet = await repository.save(walletToSave);
     return res.status(201).json(wallet);
