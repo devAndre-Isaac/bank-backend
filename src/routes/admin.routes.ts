5028379da501a6288fe9bbbedaf90b1fa26ed005
devAndre-Isaac <andreisaac344@gmail.com>
2021-10-12 17:39:50 -0300

    feat: :sparkles: create a new relatory

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,6 +1,9 @@
 import { Router, Request, Response } from "express";
+
 import AdminController from "../controllers/admin";
+import fs from "fs";
 import PDFPrinter from "pdfmake";
+import { TDocumentDefinitions } from "pdfmake/interfaces";
 
 const adminRouter = Router();
 
@@ -17,6 +20,16 @@ adminRouter.get(
       },
     };
     const printer = new PDFPrinter(fonts);
+
+    const docDefinitions: TDocumentDefinitions = {
+      defaultStyle: { font: "Helvetica" },
+      content: [{ text: "My firts relatory from code drops" }],
+    };
+
+    const pdfDoc = printer.createPdfKitDocument(docDefinitions);
+    pdfDoc.pipe(fs.createWriteStream("Relatorio.pdf"));
+    pdfDoc.end();
+    response.send("relatorio concluido");
   }
 );
 adminRouter.get("/picpay/admin/user/:id", AdminController.readUserById);
