1e703e37c43877d65788fe691b7e1d1b1fe90215
devAndre-Isaac <andreisaac344@gmail.com>
2021-10-12 17:30:49 -0300

    feat: :sparkles: add route for create pdf

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,9 +1,24 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import AdminController from "../controllers/admin";
+import PDFPrinter from "pdfmake";
 
 const adminRouter = Router();
 
-adminRouter.get("/picpay/admin/user", AdminController.readUser)
-adminRouter.get("/picpay/admin/user/:id", AdminController.readUserById)
+adminRouter.get("/picpay/admin/user", AdminController.readUser);
+adminRouter.get(
+  "/picpay/admin/user/report",
+  (request: Request, response: Response) => {
+    const fonts = {
+      Helvetica: {
+        normal: "Helvetica",
+        bold: "Helvetica-Bold",
+        italics: "Helvetica-Oblique",
+        bolditalics: "Helvetica-BoldOblique",
+      },
+    };
+    const printer = new PDFPrinter(fonts);
+  }
+);
+adminRouter.get("/picpay/admin/user/:id", AdminController.readUserById);
 
-export { adminRouter }
+export { adminRouter };
