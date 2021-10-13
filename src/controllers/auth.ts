8f48faaaf11ed8747849e8e1dfbb4c96331e13c3
devAndre-Isaac <andreisaac344@gmail.com>
2021-10-13 18:32:51 -0300

    feat: :sparkles: add httpError in auth controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -3,6 +3,7 @@ import { getMongoRepository } from "typeorm";
 import bcrypt from "bcryptjs";
 import { CommunUser } from "../entity/users";
 import jwt from "jsonwebtoken";
+import { HttpError } from "../validator/errors/HttpError";
 
 class AuthController {
   async authUser(req: Request, res: Response) {
@@ -10,11 +11,11 @@ class AuthController {
     const { email, password } = req.body;
     const validUser = await repository.findOne({ where: { email } });
     if (!validUser) {
-      return res.status(401).send("Incorrect email");
+      throw new HttpError(401, `Email: ${validUser} incorreto`);
     }
     const validPassword = await bcrypt.compare(password, validUser.password);
     if (!validPassword) {
-      return res.status(401).send("Incorrect password");
+      throw new HttpError(401, "Senha incorreta");
     }
     const token = jwt.sign({ id: validUser.id }, "secret", {
       expiresIn: "1d",
