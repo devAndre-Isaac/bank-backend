7c8847e0156816d2775897dfe592ea56d1140057
devAndre-Isaac <andreisaac344@gmail.com>
2021-10-19 21:07:59 -0300

    feat: :sparkles: add route for get transactions by cpf

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -4,6 +4,13 @@ import TransactionsController from "../controllers/transactions";
 
 const transactionsRouter = Router();
 
-transactionsRouter.post("/picpay/transactions/:id", TransactionsController.store);
+transactionsRouter.post(
+  "/picpay/transactions/:id",
+  TransactionsController.store
+);
+transactionsRouter.get(
+  "/picpay/transactions/log/:from_who_cpf",
+  TransactionsController.viewOne
+);
 
 export { transactionsRouter };
