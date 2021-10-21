6cbacc511250534cab9bd75308d4077fd9f1b7cb
devAndre-Isaac <andreisaac344@gmail.com>
2021-10-20 21:22:23 -0300

    feat: :sparkles: add depositReport middleware

---
+++ src/middlewares/depositToReport.ts
@@ -0,0 +1,27 @@
+import { getMongoRepository } from "typeorm";
+import { Movimentations } from "../entity/movimentations";
+import { CommunUser } from "../entity/users";
+
+export const DepositToReport = async (
+  id: string,
+  value: number
+) => {
+    const repositorySave = getMongoRepository(Movimentations);
+    const verifyRepositoryUser = getMongoRepository(CommunUser);
+
+    const verifyIdentification = await verifyRepositoryUser.findOne(id);
+
+    const from_who_cpf = verifyIdentification.cpf_cnpj;
+
+    const from_who = verifyIdentification.complete_name;
+
+    const userToCreate = repositorySave.create({
+        from_who_cpf,
+        from_who,
+        value,
+        type: 'deposit'
+      } as any);
+    
+      repositorySave.save(userToCreate);
+
+}
