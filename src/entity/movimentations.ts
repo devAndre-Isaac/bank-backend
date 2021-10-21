2556268c7f4d0e75b71c0059d3810dd42b0a80e9
devAndre-Isaac <andreisaac344@gmail.com>
2021-10-20 21:22:47 -0300

    feat: :sparkles: add movimentation entity

---
+++ src/entity/movimentations.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  Column,
+  ObjectIdColumn,
+  ObjectID,
+  CreateDateColumn,
+} from "typeorm";
+
+@Entity("movimentations")
+export class Movimentations {
+  @ObjectIdColumn()
+  id: ObjectID;
+
+  @Column()
+  from_who_cpf: string;
+
+  @Column()
+  from_who: string;
+
+  @Column()
+  to_who: string;
+
+  @Column()
+  value: string;
+
+  @Column()
+  type: string;
+
+  @CreateDateColumn()
+  date: Date;
+}
