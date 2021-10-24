876ffbb5c7f088dbb1cfd39290edc2f0c2c092aa
devAndre-Isaac <andreisaac344@gmail.com>
2021-10-24 00:20:34 -0300

    refactor: :recycle: remove body parser

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,15 @@ import "reflect-metadata";
 import express from "express";
 import cors from "cors";
 import "./database";
-import * as bodyParser from "body-parser";
-
 
 import { userCommunRouter } from "./routes/userCommun.routes";
 import { transactionsRouter } from "./routes/transactions.routes";
 import { adminRouter } from "./routes/admin.routes";
 import { depositRouter } from "./routes/deposit.routes";
 import { authRouter } from "./routes/auth.routes";
-// import { emailRouter } from "./routes/email.routes";
 
 const app = express();
 
-app.use(bodyParser.json())
 app.use(cors());
 app.use(express.json());
 
@@ -24,7 +20,7 @@ app.use(
   adminRouter,
   depositRouter,
   authRouter
-  );
+);
 
 app.get("/", (request, response) => {
   return response.json({ message: "PayPic - Versão 0.1" });
