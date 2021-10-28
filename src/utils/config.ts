82b645705a63f26de10c2306ac134c72329fe7a1
André Isaac <59871214+devAndre-Isaac@users.noreply.github.com>
2021-10-27 21:25:46 -0300

    Delete config.ts

--- src/utils/config.ts
+++
@@ -1,8 +0,0 @@
-class Configs {
-    public host = "smtp.gmail.com";
-    public port = 587;
-    public user = "devandreisaac@gmail.com";
-    public pass = "3443andre";
-}
-
-export default new Configs;
