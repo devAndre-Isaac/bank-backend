f4005df17d6a92848821ff9c36f75f77eafd3396
devAndre-Isaac <andreisaac344@gmail.com>
2021-10-29 18:44:34 -0300

    refactor: :recycle: update interface

--- src/interfaces/movimentations.ts
+++ src/interfaces/movimentations.ts
@@ -16,7 +16,7 @@ export interface CreateUser {
   isSeller: boolean;
 }
 
-export interface AuthUser{
-    email: string
-    password: string
+export interface AuthUser {
+  email: string;
+  password: string;
 }
