6896a521dca099ef497d972547eece46b65f1b95
devAndre-Isaac <andreisaac344@gmail.com>
2021-10-29 18:42:40 -0300

    feat: :sparkles: add interface for createUser

--- src/interfaces/transaction.ts
+++ src/interfaces/transaction.ts
@@ -1,8 +1,17 @@
 export interface SendTransaction {
-    cpf_cnpj: string;
-    value: Number;
+  cpf_cnpj: string;
+  value: Number;
 }
 
-export interface Deposit{
-    value: number
+export interface Deposit {
+  value: number;
+}
+
+export interface CreateUser {
+  complete_name: string;
+  cpf_cnpj: string;
+  email: string;
+  password: string;
+  wallet: Number;
+  isSeller: boolean;
 }
