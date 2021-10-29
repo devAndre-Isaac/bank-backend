de2b3b7a4edde97a4bc131fc7abd685736decf67
devAndre-Isaac <andreisaac344@gmail.com>
2021-10-29 19:22:43 -0300

    feat: :sparkles: insert validation for create user

--- src/controllers/userCommun.ts
+++ src/controllers/userCommun.ts
@@ -2,16 +2,24 @@ import { Request, Response } from "express";
 import { getMongoRepository } from "typeorm";
 
 import { CommunUser } from "../entity/users";
+import { createUserSchema } from "../utils/validations";
 
 class UserCommunController {
   async store(req: Request, res: Response) {
     const repository = getMongoRepository(CommunUser);
-    const { email, cpf } = req.body;
-    const userExists = await repository.findOne({ where: { email, cpf } });
+
+    const body = await createUserSchema.validate(req.body, {
+      abortEarly: false,
+      stripUnknown: true,
+    });
+
+    const { email, cpf_cnpj } = body;
+    const userExists = await repository.findOne({ where: { email, cpf_cnpj } });
+
     if (userExists) {
-      return res.status(401).send("Email já registrado no App");
+      return res.status(401).send("Credenciais já registradas no App");
     }
-    const userToSave = repository.create(req.body);
+    const userToSave = repository.create(body);
     const user = await repository.save(userToSave);
     return res.status(201).json(user);
   }
