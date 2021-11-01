059650b72a7251367748b9bc1a095bc8f24927e2
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-01 20:01:47 -0300

    feat: :sparkles: add auth middleware in admin get by id method

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import AdminController from "../controllers/admin";
+import auth from '../middlewares/auth'
 
 const adminRouter = Router();
 
 adminRouter.get("/picpay/admin/user", AdminController.readUser);
 adminRouter.get("/picpay/admin/user/report", AdminController.pdfUserGenerate);
-adminRouter.get("/picpay/admin/user/:id", AdminController.readUserById);
+adminRouter.get("/picpay/admin/user/:id", auth, AdminController.readUserById);
 
 export { adminRouter };
