9492f0208e0455fa64bf3e8782afeb39a4632bd0
devAndre-Isaac <andreisaac344@gmail.com>
2021-11-02 19:07:15 -0300

    fix: :bug: fix res.send's

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -15,15 +15,16 @@ class AuthController {
     const { email, password } = body;
     const validUser = await repository.findOne({ where: { email } });
     if (!validUser) {
-      res.status(401).send("Email incorreto");
+      return res.status(401).send("Email incorreto");
     }
-    const validPassword = await bcrypt.compare(password, validUser.password);
+    const validPassword =  await bcrypt.compare(password, validUser.password);
     if (!validPassword) {
-      res.status(401).send("Senha incorreto");
+      return res.status(401).send("Senha incorreto");
     }
     const token = jwt.sign({ id: validUser.id }, "secret", {
       expiresIn: "1d",
     });
+    delete validUser.password
     return res.json({ validUser, token });
   }
 }
